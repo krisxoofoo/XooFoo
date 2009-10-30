ef9b3a12eb8109ad319a9e0c0f85438821ca12b8
krisxoofoo <kris@xoofoo.org>
2009-10-30 11:52:13 +0000

    Ajout Paradigme Admin Gui (alpha 1)

---
+++ xoops/tags/2.4_theme_admin/htdocs/modules/system/class/gui/paradigme/language/english/admin.php
@@ -0,0 +1,23 @@
+<?php
+// $Id$
+
+define('_AD_SYSOPTIONS','System Options');
+define('_AD_INSTALLEDMODULES','Installed Modules');
+
+define('_AD_XOOPSTHEMES','XOOPS Themes');
+define('_AD_INTERESTSITES','Links');
+
+define("THEME_WELLCOME",				"Welcome");
+define("THEME_PROFILE",					"See profil");
+define("THEME_EDITPROFILE",				"Edit profil");
+define("THEME_NOTIFICATION",			"See notifications");
+define("THEME_MESSAGE",					"See messages");
+define("THEME_YOUHAVE",					"You have");
+define("THEME_NOTREAD",					"message non read");
+define("THEME_MYACCOUNT",				"My Account");
+define("THEME_SEARCH",					"Search");
+define("THEME_DESC_SEARCH",				"Search on");
+define("THEME_KEYWORDS",				"Put the keywords");
+define("THEME_UPTOP",					"Top page");
+define("THEME_USER",					"Users");
+?>
---
+++ xoops/tags/2.4_theme_admin/htdocs/modules/system/class/gui/paradigme/language/french/admin.php
@@ -0,0 +1,31 @@
+<?php
+// $Id$
+
+define("_AD_SYSOPTIONS","Options du syst&#232;me");
+define("_AD_INSTALLEDMODULES","Modules install&#233;s");
+
+define("_AD_XOOPSTHEMES","Th&#232;mes XOOPS");
+define("_AD_INTERESTSITES","Liens");
+
+
+define("THEME_WELLCOME",				"Bienvenue");
+define("THEME_PROFILE",					"Voir son profil");
+define("THEME_EDITPROFILE",				"Editer son profil");
+define("THEME_NOTIFICATION",			"Voir ses notifications");
+define("THEME_MESSAGE",					"Voir ses messages");
+define("THEME_YOUHAVE",					"Vous avez");
+define("THEME_NOTREAD",					"message(s) non lu(s)");
+define("THEME_MYACCOUNT",				"Mon compte");
+define("THEME_SEARCH",					"Recherche");
+define("THEME_DESC_SEARCH",				"Recherche sur le site");
+define("THEME_KEYWORDS",				"Saisissez les mots-cl&#233;s");
+define("THEME_UPTOP",				"Haut de page");
+define("THEME_USER",				"Utilisateurs");
+/**
+ * @translation     AFUX (Association Francophone des Utilisateurs de Xoops) <http://www.afux.org/>
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: UTF-8
+ *
+ * @version         $Id$
+**/
+?>

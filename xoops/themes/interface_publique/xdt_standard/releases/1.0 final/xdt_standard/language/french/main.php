09c75605b876e718822b740c3baa17a4d715c2ed
krisxoofoo <kris@xoofoo.org>
2010-01-08 09:16:16 +0000

    dépôt release thème xdt_standard version 1.0 final

---
+++ xoops/themes/interface_publique/xdt_standard/releases/1.0 final/xdt_standard/language/french/main.php
@@ -0,0 +1,29 @@
+<?php
+
+/* ------------------- xo_footerstatic.html ------------------- */
+define("_THEME_INBOX",            		"Voir vos messages");
+define("_THEME_INBOX_NOTREAD",    		"message(s) non lu(s)");
+define("_THEME_NOTIFICATIONS",   		"Voir vos notifications");
+define("_THEME_PROFILE",         		"Voir votre profil");
+define("_THEME_ADMINISTRATION",  		"Administration");
+define("_THEME_LOGOUT",          		"Déconnexion");
+define("THEME_EDITPROFILE",				"Editer son profil");
+define("THEME_USER",					"Utilisateurs");
+define("THEME_CPHOME",					"Administration");
+define("THEME_BANS",					"Gérer les bannières publicitaires");
+define("THEME_BKAD",					"Gé;rer les blocs");
+define("THEME_ADGS",					"Gérer les groupes");
+define("THEME_PREF",					"Préférences");
+define("THEME_INSTALLEDMODULES",		"Gérer les modules installés");
+define("THEME_IMAGES",					"Images");
+define("THEME_MDAD",					"Modules");
+define("THEME_SMLS",					"Gérer les émoticônes");
+define("THEME_RANK",					"Rang des utilisateurs");
+define("THEME_EUSER",					"Editer les utilisateurs");
+define("THEME_FINDUSER",				"Chercher un utilisateur");
+define("THEME_MLUS",					"Envoyer un courriel");
+define("THEME_AVATARS",					"Gérer les avatars");
+define("THEME_TPLS",					"Gérer les templates");
+define("THEME_COMMENTS",				"Gérer les commentaires");
+		
+?>
---
+++ xoops/themes/interface_publique/xdt_standard/releases/1.0 final/xdt_standard/language/french_numerique/main.php
@@ -0,0 +1,29 @@
+<?php
+
+/* ------------------- xo_footerstatic.html ------------------- */
+define("_THEME_INBOX",            		"Voir vos messages");
+define("_THEME_INBOX_NOTREAD",    		"message(s) non lu(s)");
+define("_THEME_NOTIFICATIONS",   		"Voir vos notifications");
+define("_THEME_PROFILE",         		"Voir votre profil");
+define("_THEME_ADMINISTRATION",  		"Administration");
+define("_THEME_LOGOUT",          		"D&#233;connexion");
+define("THEME_EDITPROFILE",				"Editer son profil");
+define("THEME_USER",					"Utilisateurs");
+define("THEME_CPHOME",					"Administration");
+define("THEME_BANS",					"G&#233;rer les banni&#232;res publicitaires");
+define("THEME_BKAD",					"G&#233;rer les blocs");
+define("THEME_ADGS",					"G&#233;rer les groupes");
+define("THEME_PREF",					"Pr&#233;f&#233;rences");
+define("THEME_INSTALLEDMODULES",		"G&#233;rer les modules install&#233;s");
+define("THEME_IMAGES",					"Images");
+define("THEME_MDAD",					"Modules");
+define("THEME_SMLS",					"G&#233;rer les &#233;motic&#244;nes");
+define("THEME_RANK",					"Rang des utilisateurs");
+define("THEME_EUSER",					"Editer les utilisateurs");
+define("THEME_FINDUSER",				"Chercher un utilisateur");
+define("THEME_MLUS",					"Envoyer un courriel");
+define("THEME_AVATARS",					"G&#233;rer les avatars");
+define("THEME_TPLS",					"G&#233;rer les templates");
+define("THEME_COMMENTS",				"G&#233;rer les commentaires");
+		
+?>

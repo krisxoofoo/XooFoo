2e9da28bfd6fc5b77c8632f11710ccf398bac13f
krisxoofoo <kris@xoofoo.org>
2010-01-12 11:45:35 +0000

    mise à jour traduction persian pour zetadigme admin gui

--- xoops/themes/interface_administration/zetadigme_1.x/trunk/language/persian/localsupport.php
+++ xoops/themes/interface_administration/zetadigme_1.x/trunk/language/persian/localsupport.php
@@ -8,21 +8,21 @@ $menu[] = array(
     'link'      => 'http://www.xoops.org',
     'title'     => 'زوپس انگلیسی',
     'absolute'  => 1,
-    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
 );
 
 $menu[] = array(
     'link'      => 'http://www.irxoops.org',
     'title'     => 'زوپس فارسی',
     'absolute'  => 1,
-    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
 );
 
 $menu[] = array(
     'link'      => 'http://www.irxoops.org/modules/TDMDownloads/index.php',
     'title'     => _AD_XOOPSMODULES,
     'absolute'  => 1,
-    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
 );
 
 

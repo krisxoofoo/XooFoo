d47a8fc5ae051740bb9a0c1854d9763f8669b547
krisxoofoo <kris@xoofoo.org>
2010-02-13 11:37:19 +0000

    Dépôt Suico theme (alpha 2 release) for Xoops 2.4.5+

---
+++ xoops/themes/interface_publique/suico/trunk/themes/suico/language/italian/main.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * Suico theme
+ * In memory of Marcello Brandao
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license 3.0
+ * @package         themes
+ * @since           2.5.x
+ * @author          Kris <http://www.xoofoo.org/>
+ * @maintained      The XOOPS Project http://sourceforge.net/projects/xoops/
+ *
+ * @version         $Id
+*/
+
+// system_notification_select.html
+define("THEME_SHOWHIDE_NOTIFICATION",	"Show / Hide");
+define("THEME_SHOWHIDE_NOTIFICATION_DESC",	"Show or hide the notification options");
+define("THEME_RSS",					"RSS Feeds");
+define("THEME_SEARCH",					"Cerca");
+define("THEME_DESC_SEARCH",				"Ricerca questo sito");
+define("THEME_KEYWORDS",				"Invia keywords");
+
+// blockszone and centerblocks.html
+define("THEME_THISBLOCK_EDIT",			"Edit this block");
+define("THEME_VIEWALL",					"View All");
+
+/* ------------------- xo_globalnav.html ---------------------- */
+define("THEME_NAVTITLE",				"Navigatione");
+
+define("THEME_HOME",					"Home");
+define("THEME_HOME_DESC",				"Back to home");
+
+define("THEME_MODULE1",					"Notizie");
+define("THEME_MODULE1_DESC",				"Veda le notizie");
+
+define("THEME_MODULE2",					"BBS");
+define("THEME_MODULE2_DESC",			"Visiti la nostro forum");
+
+define("THEME_MODULE3",					"Galleria");
+define("THEME_MODULE3_DESC",			"Visiti la nostra galleria della foto");
+
+define("THEME_MODULE4",					"Film");
+define("THEME_MODULE4_DESC",			"Veda i nostri film");
+
+define("THEME_MODULE5",					"Archivio Link");
+define("THEME_MODULE5_DESC",			"Visiti i nostri web links");
+
+define("THEME_MODULE6",					"Downloads");
+define("THEME_MODULE6_DESC",			"Download le nostre lime");
+
+define("THEME_MODULE7",					"");
+define("THEME_MODULE7_DESC",			"");
+
+define("THEME_MODULE8",					"");
+define("THEME_MODULE8_DESC",			"");
+
+define("THEME_MODULE9",					"");
+define("THEME_MODULE9_DESC",			"");
+
+// noms des modules dans globalnav.html en mode anonyme
+define("THEME_MODULE1a",				"Accreditamenti");
+define("THEME_MODULE1a_DESC",			"Accreditamenti del Sito");
+define("THEME_MODULE2a",				"Localizzazione");
+define("THEME_MODULE2a_DESC",			"Localizzili su una mapa");
+
+define("THEME_CONTACT",					"Contatto");
+define("THEME_CONTACT_DESC",				"Contattaci?");
+define("THEME_SITEMAP",					"Sitemap");
+
+// xo_userbar.html
+define("THEME_REGISTER",				"Registrazione utente");
+define("THEME_REMEMBER",				" Ricordi da me?");
+define("THEME_PSEUDO",					" Nome utente");
+define("THEME_DESC_PSEUDO",				" Invia utente");
+define("THEME_PASSWORD",				" Password");
+define("THEME_DESC_PASSWORD",				" Invia password");
+define("THEME_CONNECT",					"Connexion");
+define("THEME_LOSTACCOUNT",				"Mot de passe perdu ?"); // add for suico theme
+define("THEME_CLOSEPOPUP",				"Fermer la fenêtre"); // add for suico theme
+
+define("THEME_WELLCOME",				"Benvenuto");
+define("THEME_PROFILE",					"Mostra profilo");
+define("THEME_NOTIFICATION",				"Notifiche");
+define("THEME_MESSAGE",					"Messaggi in arrivo");
+define("THEME_YOUHAVE",					"Tu hai");
+define("THEME_NOTREAD",					"nuovi messaggi");
+define("THEME_MYACCOUNT",				"Mio profilo");
+define("THEME_PRIVATE",					"Private Access");
+
+define("THEME_UFROM",					"Location"); // add for suico theme
+define("THEME_UOCCUPAT",				"Occupation"); // add for suico theme
+define("THEME_UINTEREST",				"Interest"); // add for suico theme
+define("THEME_UINFOS",					"Informations"); // add for suico theme
+define("THEME_UREGDATE",				"Member since"); // add for suico theme
+define("THEME_UPOSTS",					"Posts Number"); // add for suico theme
+define("THEME_ULASTLOGIN",				"Last login"); // add for suico theme
+
+define("THEME_UEDITPROFILE",			"Edit profile"); // add for suico theme
+define("THEME_UEDITAVATAR",				"Change image"); // add for suico theme
+define("THEME_UEDITPASSWORD",			"Change password"); // add for suico theme
+
+/* ------------------- xo_footerstatic.html ------------------- */
+define("_THEME_INBOX",            		"See your messages");
+define("_THEME_INBOX_NOTREAD",    		"message(s) no read");
+define("_THEME_NOTIFICATIONS",   		"See notifications");
+define("_THEME_PROFILE",         		"See profil");
+define("_THEME_ADMINISTRATION",  		"Administration");
+define("_THEME_LOGOUT",          		"Logout");
+define("THEME_EDITPROFILE",				"Edit profil");
+define("THEME_USER",					"Users");
+define("THEME_CPHOME",					"Administration");
+define("THEME_BANS",					"Manage banners");
+define("THEME_BKAD",					"Manage blocks");
+define("THEME_ADGS",					"Manage users groups");
+define("THEME_PREF",					"Settings");
+define("THEME_INSTALLEDMODULES",		"Manage module(s)");
+define("THEME_IMAGES",					"Images");
+define("THEME_MDAD",					"Modules");
+define("THEME_SMLS",					"Manage smilies");
+define("THEME_RANK",					"User Ranks");
+define("THEME_EUSER",					"Edit User(s)");
+define("THEME_FINDUSER",				"Find a user");
+define("THEME_MLUS",					"Send email");
+define("THEME_AVATARS",					"Manage avatars");
+define("THEME_TPLS",					"Manage templates");
+define("THEME_COMMENTS",				"Manage comments");
+
+?>

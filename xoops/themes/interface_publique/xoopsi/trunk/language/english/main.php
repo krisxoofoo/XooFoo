a82f0c07685173ed799024f09faa945fab76057f
krisxoofoo <kris@xoofoo.org>
2010-03-08 14:58:38 +0000

    add missing define language in xoopsi theme

--- xoops/themes/interface_publique/xoopsi/trunk/language/english/main.php
+++ xoops/themes/interface_publique/xoopsi/trunk/language/english/main.php
@@ -12,17 +12,92 @@
  * @version         $Id
 */
 
+// system_notification_select.html
+define("THEME_SHOWHIDE_NOTIFICATION",	"Show / Hide");
+define("THEME_SHOWHIDE_NOTIFICATION_DESC",	"Show or hide the notification options");
 define("THEME_RSS",						"Feed RSS");
-define("THEME_FANFACE",					"Join us on Facebook");
-define("THEME_TWITME",					"Follow our Twitts!");
+define("THEME_SEARCH",					"Search");
+define("THEME_DESC_SEARCH",				"Search this site");
+define("THEME_KEYWORDS",				"Put the keywords");
 
+// blockszone and centerblocks.html
+define("THEME_THISBLOCK_EDIT",			"Edit this block");
+define("THEME_VIEWALL",					"View All");
+
+// xo_globalnav.html
+define("THEME_NAVTITLE",				"Navigation");
+
+define("THEME_HOME",					"Home");
+define("THEME_HOME_DESC",				"Back to home");
+
+define("THEME_MODULE1",					"News");
+define("THEME_MODULE1_DESC",			"See news");
+
+define("THEME_MODULE2",					"BBS");
+define("THEME_MODULE2_DESC",			"Visit our Forums");
+
+define("THEME_MODULE3",					"Gallery");
+define("THEME_MODULE3_DESC",			"Visit our Photo Gallery");
+
+define("THEME_MODULE4",					"Movies");
+define("THEME_MODULE4_DESC",			"See our movies");
+
+define("THEME_MODULE5",					"Web Links");
+define("THEME_MODULE5_DESC",			"Visit our web links");
+
+define("THEME_MODULE6",					"Downloads");
+define("THEME_MODULE6_DESC",			"Download our files");
+
+define("THEME_MODULE7",					"");
+define("THEME_MODULE7_DESC",			"");
+
+define("THEME_MODULE8",					"");
+define("THEME_MODULE8_DESC",			"");
+
+define("THEME_MODULE9",					"");
+define("THEME_MODULE9_DESC",			"");
+
+// module name in xo_globalnav.html  anomymous mode
+define("THEME_MODULE1a",				"Credits");
+define("THEME_MODULE1a_DESC",			"Website Credits");
+define("THEME_MODULE2a",				"Localization");
+define("THEME_MODULE2a_DESC",			"Localize us on a map");
+
+define("THEME_CONTACT",					"Contact");
+define("THEME_CONTACT_DESC",			"Contact us ?");
+define("THEME_SITEMAP",					"Sitemap");
+
+// xo_userbar.html
+define("THEME_REGISTER",				"Create account ?");
+define("THEME_REMEMBER",				"Remember me ?");
+define("THEME_PSEUDO",					"Username");
+define("THEME_DESC_PSEUDO",				"Put your username");
+define("THEME_PASSWORD",				"Password");
+define("THEME_DESC_PASSWORD",			"Put your password");
+define("THEME_CONNECT",					"Connexion");
+define("THEME_LOSTACCOUNT",				"Password lost ?"); // add for suico theme
+define("THEME_CLOSEPOPUP",				"Close"); // add for suico theme
+
+define("THEME_WELLCOME",				"Wellcome");
 define("THEME_PROFILE",					"See profil");
 define("THEME_NOTIFICATION",			"See notifications");
 define("THEME_MESSAGE",					"See private messages");
 define("THEME_YOUHAVE",					"You have");
 define("THEME_NOTREAD",					"message(s) no read");
 define("THEME_MYACCOUNT",				"My Account");
-define("THEME_UEDITPROFILE",			"Edit profile");
+define("THEME_PRIVATE",					"Private Access");
+
+define("THEME_UFROM",					"Location"); // add for suico theme
+define("THEME_UOCCUPAT",				"Occupation"); // add for suico theme
+define("THEME_UINTEREST",				"Interest"); // add for suico theme
+define("THEME_UINFOS",					"Informations"); // add for suico theme
+define("THEME_UREGDATE",				"Member since"); // add for suico theme
+define("THEME_UPOSTS",					"Posts Number"); // add for suico theme
+define("THEME_ULASTLOGIN",				"Last login"); // add for suico theme
+
+define("THEME_UEDITPROFILE",			"Edit profile"); // add for suico theme
+define("THEME_UEDITAVATAR",				"Change image"); // add for suico theme
+define("THEME_UEDITPASSWORD",			"Change password"); // add for suico theme
 
 /* ------------------- xo_footerstatic.html ------------------- */
 define("_THEME_INBOX",            		"See your messages");
@@ -49,10 +124,10 @@ define("THEME_MLUS",					"Send email");
 define("THEME_AVATARS",					"Manage avatars");
 define("THEME_TPLS",					"Manage templates");
 define("THEME_COMMENTS",				"Manage comments");
-define("THEME_ADMTOOLS",				"Tools");
-define("THEME_ADMFILES",				"Files Manager");
-define("THEME_ADMCLEAN",				"Maintenance");
-define("THEME_SYSSETTING",				"System Module Settings");
-define("THEME_UPTOP",					"Top Page");
+define("THEME_ADMTOOLS",				"Tools"); // add for xoops 2.5.0
+define("THEME_ADMFILES",				"Files Manager");  // add for xoops 2.5.0
+define("THEME_ADMCLEAN",				"Maintenance");  // add for xoops 2.5.0
+define("THEME_SYSSETTING",				"System Module Settings");  // add for xoops 2.5.0
+define("THEME_UPTOP",					"Top Page"); // add for xoops 2.5.0
 
 ?>

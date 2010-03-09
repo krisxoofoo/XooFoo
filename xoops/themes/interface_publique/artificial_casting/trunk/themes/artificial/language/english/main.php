d60501969f8cad4b6678d2c4109bb27fcdef40b9
krisxoofoo <kris@xoofoo.org>
2010-03-09 22:38:34 +0000

    add Artificial Casting theme

---
+++ xoops/themes/interface_publique/artificial_casting/trunk/themes/artificial/language/english/main.php
@@ -0,0 +1,65 @@
+<?php
+// xo_userbar.html
+define("THEME_REGISTER",				"Create account ?");
+define("THEME_REMEMBER",				"Remember me ?");
+define("THEME_PSEUDO",					"Username");
+define("THEME_DESC_PSEUDO",				"Put your username");
+define("THEME_PASSWORD",				"Password");
+define("THEME_DESC_PASSWORD",			"Put your password");
+define("THEME_LOSTACCOUNT",				"Password lost ?");
+define("THEME_OK",						"OK");
+
+define("THEME_UEDITPROFILE",			"Edit profile");
+define("THEME_UEDITAVATAR",				"Change image?");
+define("THEME_UEDITPASSWORD",			"Change password?");
+define("THEME_UPOSTS",					"Posts Number");
+
+define("THEME_WELLCOME",				"Wellcome");
+define("THEME_PROFILE",					"See profil");
+define("THEME_NOTIFICATION",			"See notifications");
+define("THEME_MESSAGE",					"See private messages");
+define("THEME_YOUHAVE",					"You have");
+define("THEME_NOTREAD",					"message(s) no read");
+define("THEME_MYACCOUNT",				"My Account");
+
+define("THEME_DESC_SEARCH",				"Search this site");
+define("THEME_KEYWORDS",				"Put the keywords");
+
+define("THEME_CONTACT",					"Contact");
+define("THEME_CONTACT_DESC",			"Contact us ?");
+define("THEME_SITEMAP",					"Sitemap");
+
+define("THEME_HOME",					"Home");
+define("THEME_HOME_DESC",				"Back to home");
+
+define("THEME_MODULE1",					"News");
+define("THEME_MODULE1_DESC",			"See news");
+
+define("THEME_MODULE2",					"BBS");
+define("THEME_MODULE2_DESC",			"Visit our Forums");
+
+define("THEME_MODULE3",					"Gallery");
+define("THEME_MODULE3_DESC",			"Visit our Photo Gallery");
+
+define("THEME_MODULE4",					"Movies");
+define("THEME_MODULE4_DESC",			"See our movies");
+
+define("THEME_MODULE5",					"Web Links");
+define("THEME_MODULE5_DESC",			"Visit our web links");
+
+define("THEME_MODULE6",					"Downloads");
+define("THEME_MODULE6_DESC",			"Download our files");
+
+define("THEME_MODULE7",					"XoopsInfo");
+define("THEME_MODULE7_DESC",			"Use XoopsInfo Module");
+
+define("THEME_MODULE8",					"Protector");
+define("THEME_MODULE8_DESC",			"Use Protector Module");
+
+// module name in xo_globalnav.html  anomymous mode
+define("THEME_MODULE1a",				"Credits");
+define("THEME_MODULE1a_DESC",			"Website Credits");
+define("THEME_MODULE2a",				"Localization");
+define("THEME_MODULE2a_DESC",			"Localize us on a map");
+
+?>

9ed97f7f8d2a4d016bc3e8c1e5e488aa4f5b06e0
krisxoofoo <kris@xoofoo.org>
2010-03-09 12:31:24 +0000

    add dusty theme

---
+++ xoops/themes/interface_publique/dusty/trunk/language/english/main.php
@@ -0,0 +1,43 @@
+<?php
+
+define("THEME_DESC_SEARCH",				"Search this site");
+define("THEME_KEYWORDS",				"Put the keywords");
+
+define("THEME_CONTACT",					"Contact");
+define("THEME_CONTACT_DESC",			"Contact us ?");
+define("THEME_SITEMAP",					"Sitemap");
+
+define("THEME_HOME",					"Home");
+define("THEME_HOME_DESC",				"Back to home");
+
+define("THEME_MODULE1",					"News");
+define("THEME_MODULE1_DESC",			"See news");
+
+define("THEME_MODULE2",					"BBS");
+define("THEME_MODULE2_DESC",			"Visit our Forums");
+
+define("THEME_MODULE3",					"Gallery");
+define("THEME_MODULE3_DESC",			"Visit our Photo Gallery");
+
+define("THEME_MODULE4",					"Movies");
+define("THEME_MODULE4_DESC",			"See our movies");
+
+define("THEME_MODULE5",					"Web Links");
+define("THEME_MODULE5_DESC",			"Visit our web links");
+
+define("THEME_MODULE6",					"Downloads");
+define("THEME_MODULE6_DESC",			"Download our files");
+
+define("THEME_MODULE7",					"XoopsInfo");
+define("THEME_MODULE7_DESC",			"Use XoopsInfo Module");
+
+define("THEME_MODULE8",					"Protector");
+define("THEME_MODULE8_DESC",			"Use Protector Module");
+
+// module name in xo_globalnav.html  anomymous mode
+define("THEME_MODULE1a",				"Credits");
+define("THEME_MODULE1a_DESC",			"Website Credits");
+define("THEME_MODULE2a",				"Localization");
+define("THEME_MODULE2a_DESC",			"Localize us on a map");
+
+?>
---
+++ xoops/themes/interface_publique/dusty/trunk/language/french_iso/main.php
@@ -0,0 +1,43 @@
+<?php
+
+define("THEME_DESC_SEARCH",				"Recherche sur le site");
+define("THEME_KEYWORDS",				"Saisissez les mots-clés");
+
+define("THEME_CONTACT",					"Contact");
+define("THEME_CONTACT_DESC",			"Nous contacter ?");
+define("THEME_SITEMAP",					"Plan du site");
+
+define("THEME_HOME",					"Accueil");
+define("THEME_HOME_DESC",				"Revenir à l'accueil");
+
+define("THEME_MODULE1",					"Articles");
+define("THEME_MODULE1_DESC",			"Voir les news");
+
+define("THEME_MODULE2",					"Forum");
+define("THEME_MODULE2_DESC",			"Visiter les Forums du site");
+
+define("THEME_MODULE3",					"Galerie");
+define("THEME_MODULE3_DESC",			"Visiter les Galeries Photos");
+
+define("THEME_MODULE4",					"Vidéos");
+define("THEME_MODULE4_DESC",			"Regarder les Vidéos publiées");
+
+define("THEME_MODULE5",					"Liens Web");
+define("THEME_MODULE5_DESC",			"Visiter nos Liens web favoris");
+
+define("THEME_MODULE6",					"Téléchargements");
+define("THEME_MODULE6_DESC",			"Télécharger nos fichiers");
+
+define("THEME_MODULE7",					"XoopsInfo");
+define("THEME_MODULE7_DESC",			"Utiliser le module XoopsInfo");
+
+define("THEME_MODULE8",					"Protector");
+define("THEME_MODULE8_DESC",			"Utiliser Protector");
+
+// noms des modules dans globalnav.html en mode anonyme
+define("THEME_MODULE1a",				"Crédits");
+define("THEME_MODULE1a_DESC",			"Crédits à la réalisation de ce site");
+define("THEME_MODULE2a",				"Localisation");
+define("THEME_MODULE2a_DESC",			"Nous localiser sur une carte");
+
+?>

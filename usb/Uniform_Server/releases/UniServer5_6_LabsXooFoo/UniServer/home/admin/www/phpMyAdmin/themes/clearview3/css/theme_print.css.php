3e8878fda87e6d1e8985c68208ff24d638b901c2
krisxoofoo <kris@xoofoo.org>
2010-03-13 12:28:02 +0000

    Ajout Pack personnalisation Labs.XooFoo pour UniServer 5.6

---
+++ usb/Uniform_Server/releases/UniServer5_6_LabsXooFoo/UniServer/home/admin/www/phpMyAdmin/themes/clearview3/css/theme_print.css.php
@@ -0,0 +1,39 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * print css file from theme ClearView3
+ *
+ * @version $Id: theme_print.css.php 12180 2009-01-10 14:10:35Z lem9 $
+ * @package phpMyAdmin-theme
+ * @subpackage ClearView3
+ */
+?>
+/* For printview */
+body, table, th, td {
+    color:             #000000;
+    background-color:  #ffffff;
+}
+
+img {
+    border: 0;
+}
+
+table, th, td {
+    border-width:      0.1em;
+    border-color:      #000000;
+    border-style:      solid;
+}
+
+table {
+    border-collapse:   collapse;
+    border-spacing:    0;
+}
+
+th, td {
+    padding:           0.2em;
+}
+
+th {
+    font-weight:       bold;
+    background-color:  #e5e5e5;
+}
---
+++ usb/Uniform_Server/releases/UniServer5_6_LabsXooFoo/UniServer/home/admin/www/phpMyAdmin/themes/clearview3/info.inc.php
@@ -0,0 +1,16 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ * @version $Id: info.inc.php 12180 2009-01-10 14:10:35Z lem9 $
+ * @package phpMyAdmin-theme
+ * @subpackage Original
+ */
+
+/**
+ *
+ */
+$theme_name = 'ClearView 3';
+$theme_full_version = '3.0';
+?>

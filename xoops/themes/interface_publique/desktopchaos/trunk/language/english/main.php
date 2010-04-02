2cdeb242950f64f0bee64977152aaa5d349ba6e0
krisxoofoo <kris@xoofoo.org>
2010-04-02 17:55:31 +0000

    Ajout de DesktopChaos theme

---
+++ xoops/themes/interface_publique/desktopchaos/trunk/language/english/main.php
@@ -0,0 +1,114 @@
+<?php
+define("THEME_RSS",						"RSS Feed");
+
+// xo_userbar.html
+define("THEME_REGISTER",				"Create account ?");
+define("THEME_REMEMBER",				"Remember me ?");
+define("THEME_PSEUDO",					"Username");
+define("THEME_DESC_PSEUDO",				"Put your username");
+define("THEME_PASSWORD",				"Password");
+define("THEME_DESC_PASSWORD",			"Put your password");
+define("THEME_LOSTACCOUNT",				"Password lost ?");
+define("THEME_OK",						"OK");
+
+define("THEME_UEDITPROFILE",			"Edit profile");
+define("THEME_UEDITAVATAR",				"Change image?");
+define("THEME_UEDITPASSWORD",			"Change password?");
+define("THEME_UPOSTS",					"Posts Number");
+
+define("THEME_WELLCOME",				"Wellcome");
+define("THEME_PROFILE",					"See profil");
+define("THEME_NOTIFICATION",			"See notifications");
+define("THEME_MESSAGE",					"See private messages");
+define("THEME_YOUHAVE",					"You have");
+define("THEME_NOTREAD",					"message(s) no read");
+define("THEME_MYACCOUNT",				"My Account");
+
+define("THEME_DESC_SEARCH",				"Search this site");
+define("THEME_KEYWORDS",				"Put the keywords");
+
+define("THEME_CONTACT",					"Contact");
+define("THEME_CONTACT_DESC",			"Contact us ?");
+define("THEME_SITEMAP",					"Sitemap");
+
+define("THEME_HOME",					"Home");
+define("THEME_HOME_DESC",				"Back to home");
+
+define("THEME_MODULE1",					"News");
+define("THEME_MODULE1_DESC",			"See news");
+
+define("THEME_MODULE2",					"BBS");
+define("THEME_MODULE2_DESC",			"Visit our Forums");
+
+define("THEME_MODULE3",					"Gallery");
+define("THEME_MODULE3_DESC",			"Visit our Photo Gallery");
+
+define("THEME_MODULE4",					"Movies");
+define("THEME_MODULE4_DESC",			"See our movies");
+
+define("THEME_MODULE5",					"Web Links");
+define("THEME_MODULE5_DESC",			"Visit our web links");
+
+define("THEME_MODULE6",					"Downloads");
+define("THEME_MODULE6_DESC",			"Download our files");
+
+define("THEME_MODULE7",					"XoopsInfo");
+define("THEME_MODULE7_DESC",			"Use XoopsInfo Module");
+
+define("THEME_MODULE8",					"Protector");
+define("THEME_MODULE8_DESC",			"Use Protector Module");
+
+// module name in xo_globalnav.html  anomymous mode
+define("THEME_MODULE1a",				"Credits");
+define("THEME_MODULE1a_DESC",			"Website Credits");
+define("THEME_MODULE2a",				"Localization");
+define("THEME_MODULE2a_DESC",			"Localize us on a map");
+
+/* ------------------- xo_socialbookmark.html ------------------- */
+define("THEME_BK_SCRIPSTYLE",          	"Submit this to Script & Style");
+define("THEME_BK_BLINKLIST",           	"Share this on Blinklist");
+define("THEME_BK_DELICIOUS",           	"Share this on del.icio.us");
+define("THEME_BK_DIGG",            		"Digg this!");
+define("THEME_BK_FURL",            		"Share this on Furl");
+define("THEME_BK_REDDIT",            	"Share this on Reddit");
+define("THEME_BK_YAHOO",            	"Save this to Yahoo MyWeb");
+define("THEME_BK_STUMBLE",            	"Stumble upon something good? Share it on StumbleUpon");
+define("THEME_BK_TWITTER",            	"Tweet This!");
+define("THEME_BK_TECHNORATI",          	"Share this on Technorati");
+define("THEME_BK_MIXX",            		"Share this on Mixx");
+define("THEME_BK_MYSPACE",            	"Post this to MySpace");
+define("THEME_BK_DESIGNFLOAT",         	"Submit this to DesignFloat");
+define("THEME_BK_FACEBOOK",            	"Share this on Facebook");
+
+/* ------------------- xo_footerstatic.html ------------------- */
+define("_THEME_INBOX",            		"See your messages");
+define("_THEME_INBOX_NOTREAD",    		"message(s) no read");
+define("_THEME_NOTIFICATIONS",   		"See notifications");
+define("_THEME_PROFILE",         		"See profil");
+define("_THEME_ADMINISTRATION",  		"Administration");
+define("_THEME_LOGOUT",          		"Logout");
+define("THEME_EDITPROFILE",				"Edit profil");
+define("THEME_USER",					"Users");
+define("THEME_CPHOME",					"Administration");
+define("THEME_BANS",					"Manage banners");
+define("THEME_BKAD",					"Manage blocks");
+define("THEME_ADGS",					"Manage users groups");
+define("THEME_PREF",					"Settings");
+define("THEME_INSTALLEDMODULES",		"Manage module(s)");
+define("THEME_IMAGES",					"Images");
+define("THEME_MDAD",					"Modules");
+define("THEME_SMLS",					"Manage smilies");
+define("THEME_RANK",					"User Ranks");
+define("THEME_EUSER",					"Edit User(s)");
+define("THEME_FINDUSER",				"Find a user");
+define("THEME_MLUS",					"Send email");
+define("THEME_AVATARS",					"Manage avatars");
+define("THEME_TPLS",					"Manage templates");
+define("THEME_COMMENTS",				"Manage comments");
+define("THEME_ADMTOOLS",				"Tools");
+define("THEME_ADMFILES",				"Files Manager");
+define("THEME_ADMCLEAN",				"Maintenance");
+define("THEME_SYSSETTING",				"System Module Settings");
+define("THEME_UPTOP",					"Top Page");
+
+?>
---
+++ xoops/themes/interface_publique/desktopchaos/trunk/language/french_iso/main.php
@@ -0,0 +1,36 @@
+<?php
+define("THEME_RSS",						"Alimentations du fil RSS");
+
+// xo_userbar.html
+define("THEME_REGISTER",				"Créer un compte ?");
+define("THEME_REMEMBER",				"Se rappeller de moi ?");
+define("THEME_PSEUDO",					"Identifiant");
+define("THEME_DESC_PSEUDO",				"Saisissez votre identifiant");
+define("THEME_PASSWORD",				"Password");
+define("THEME_DESC_PASSWORD",			"Saisissez votre mot de passe");
+define("THEME_LOSTACCOUNT",				"Perdu le mot de passe ?");
+define("THEME_OK",						"OK");
+
+define("THEME_DESC_SEARCH",				"Recherche sur le site");
+define("THEME_KEYWORDS",				"Saisissez les mots-clés");
+
+define("THEME_HOME",					"Accueil");
+define("THEME_HOME_DESC",				"Revenir à l'accueil");
+
+/* ------------------- xo_socialbookmark.html ------------------- */
+define("THEME_BK_SCRIPSTYLE",          	"Soumettre ceci sur Script & Style");
+define("THEME_BK_BLINKLIST",           	"Suivre ceci sur Blinklist");
+define("THEME_BK_DELICIOUS",           	"Suivre ceci sur del.icio.us");
+define("THEME_BK_DIGG",            		"Digg ceci !");
+define("THEME_BK_FURL",            		"Suivre ceci sur Furl");
+define("THEME_BK_REDDIT",            	"Suivre ceci sur Reddit");
+define("THEME_BK_YAHOO",            	"Sauvegarder sur Yahoo MyWeb");
+define("THEME_BK_STUMBLE",            	"Suivre ceci sur StumbleUpon");
+define("THEME_BK_TWITTER",            	"Tweet ceci !");
+define("THEME_BK_TECHNORATI",          	"Suivre ceci sur Technorati");
+define("THEME_BK_MIXX",            		"Suivre ceci sur Mixx");
+define("THEME_BK_MYSPACE",            	"Poster sur MySpace");
+define("THEME_BK_DESIGNFLOAT",         	"Soumettre ceci sur DesignFloat");
+define("THEME_BK_FACEBOOK",            	"Suivre ceci sur Facebook");
+
+?>

26e32229d863b9ec8b05cabc9d17e3b885506501
krisxoofoo <kris@xoofoo.org>
2010-09-03 12:36:51 +0000

    création dossier pour pack fichiers UniformServer 5.6.12 LabsXooFoo revisited

---
+++ usb/Uniform_Server/releases/UniServer5_6_12_LabsXooFoo_revisited/UniServer/home/admin/www/phpMyAdmin/themes/clearview3/info.inc.php
@@ -0,0 +1,16 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ * @version $Id: info.inc.php 351 2010-08-29 16:11:47Z kris_fr $
+ * @package phpMyAdmin-theme
+ * @subpackage Original
+ */
+
+/**
+ *
+ */
+$theme_name = 'ClearView 3';
+$theme_full_version = '3.0';
+?>

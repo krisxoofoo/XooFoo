dfaa119cf6696dd6d1042010f248ee1fc9ff9fae
krisxoofoo <kris@xoofoo.org>
2010-09-13 20:41:23 +0000

    dépôt release module mysqledit 1.0 RC

---
+++ xoops/modules/mysqledit/releases/1.0rc/modules/mysqledit/language/french_iso/modinfo.php
@@ -0,0 +1,19 @@
+<?php
+// Module Info
+
+// The name of this module
+define("_MI_MYSQLEDIT_NAME","MySQL Editor");
+
+// A brief description of this module
+define("_MI_MYSQLEDIT_DESC","Editeur de base de données pour Xoops");
+
+/**
+ * @translation     Communauté Francophone des Utilisateurs de Xoops
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: ISO-8859-1
+ *
+ * @Translator      kris (http://www.xoofoo.org)
+ *
+ * @version         $Id $
+**/
+?>

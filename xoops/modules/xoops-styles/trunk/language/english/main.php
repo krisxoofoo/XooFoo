ff1474fb7af118155a5f2f90f6209ea07b0b7038
krisxoofoo <kris@xoofoo.org>
2010-09-24 15:21:49 +0000

    mise à jour trunk du module xoops-styles

--- xoops/modules/xoops-styles/trunk/language/english/main.php
+++ xoops/modules/xoops-styles/trunk/language/english/main.php
@@ -1,6 +1,27 @@
 <?php
 /* @version             $Id $*/
 
+/* general */
+define("_MD_XOSTYLE_WITHTXT","with text");
+define("_MD_XOSTYLE_WITHIMG","with image");
+define("_MD_XOSTYLE_WITHLINK","with hypertext link");
+define("_MD_XOSTYLE_WITHOUT","without style (default)");
+define("_MD_XOSTYLE_LINE1","Line 1");
+define("_MD_XOSTYLE_LINE2","Line 2");
+define("_MD_XOSTYLE_LINE3","Line 3");
+define("_MD_XOSTYLE_CELL1","Cell 1");
+define("_MD_XOSTYLE_CELL2","Cell 2");
+define("_MD_XOSTYLE_CELL3","Cell 3");
+define("_MD_XOSTYLE_TAG","Tag");
+define("_MD_XOSTYLE_IMG","Image");
+define("_MD_XOSTYLE_IMGLINK","Image with hypertext link");
+define("_MD_XOSTYLE_OR","or");
+define("_MD_XOSTYLE_AND","and");
+define("_MD_XOSTYLE_LOREM","Lorem ipsum");
+define("_MD_XOSTYLE_LOREMLINK","Lorem ipsum with hypertext link");
+define("_MD_XOSTYLE_DEPREC","<span class='red smallsmall italic'>(Warning ! Deprecead !!)</span>");
+define("_MD_XOSTYLE_TITTABLE","Table");
+
 /* xostyle_xoopscss.html */
 define("_MD_XOSTYLE_TITLE","Generic Css Class in xoops.css");
 define("_MD_XOSTYLE_TITTXT","Text");
@@ -54,20 +75,66 @@ define("_MD_XOSTYLE_TITRNDBORDER","Round Border");
 define("_MD_XOSTYLE_RNDBORDER_TXT","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ante fermentum diam iaculis et pulvinar metus pharetra. Sed sed nisi eu metus viverra aliquam at eleifend sapien.");
 define("_MD_XOSTYLE_TITOPAC","Opacity");
 define("_MD_XOSTYLE_OPAC_TXT","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ante fermentum diam iaculis et pulvinar metus pharetra. Sed sed nisi eu metus viverra aliquam at eleifend sapien.");
-define("_MD_XOSTYLE_TITTABLE","Table");
 
-define("_MD_XOSTYLE_WITHTXT","with text");
-define("_MD_XOSTYLE_WITHIMG","with image");
-define("_MD_XOSTYLE_WITHLINK","with hypertext link");
-define("_MD_XOSTYLE_WITHOUT","without style (default)");
-define("_MD_XOSTYLE_LINE1","Line 1");
-define("_MD_XOSTYLE_LINE2","Line 2");
-define("_MD_XOSTYLE_LINE3","Line 3");
-define("_MD_XOSTYLE_CELL1","Cell 1");
-define("_MD_XOSTYLE_CELL2","Cell 2");
-define("_MD_XOSTYLE_CELL3","Cell 3");
 /* xostyle_theme.html */
 define("_MD_XOSTYLE_TITLE1","Style of the current theme");
+define("_MD_XOSTYLE_TAGH","Tag \"h1 - h6\"");
+define("_MD_XOSTYLE_TAGHLINK","Tag \"h1 - h6\" with hypertext link");
+define("_MD_XOSTYLE_TITLEH1","Title h1");
+define("_MD_XOSTYLE_TITLEH2","Title h2");
+define("_MD_XOSTYLE_TITLEH3","Title h3");
+define("_MD_XOSTYLE_TITLEH4","Title h4");
+define("_MD_XOSTYLE_TITLEH5","Title h5");
+define("_MD_XOSTYLE_TITLEH6","Title h6");
+define("_MD_XOSTYLE_FONTGEN","Generic font class");
+define("_MD_XOSTYLE_GENEL","General elements");
+define("_MD_XOSTYLE_PAGINATION","Pagination");
+define("_MD_XOSTYLE_TAGLI","Tag \"li\"");
+define("_MD_XOSTYLE_TAGOL","Tag \"ol\"");
+define("_MD_XOSTYLE_COREMSG","Core Messages");
+define("_MD_XOSTYLE_COREMSGLINK","Core Messages with hypertext link");
+define("_MD_XOSTYLE_TABLETAG","Table Tag");
+define("_MD_XOSTYLE_TABLETAGLINK","Table Tag with hypertext link");
+define("_MD_XOSTYLE_TABLEROW","Tables row class");
+define("_MD_XOSTYLE_TABLEROWLINK","Tables row class with hypertext link");
+define("_MD_XOSTYLE_TABLECLASS","Table Class");
+define("_MD_XOSTYLE_TABLECLASSLINK","Table Class with hypertext link");
+define("_MD_XOSTYLE_COMCLASS","Comments class");
+define("_MD_XOSTYLE_COMCLASSLINK","Comments class with hypertext link");
+define("_MD_XOSTYLE_PARAF","Paragraph style & text selection");
+define("_MD_XOSTYLE_PARAFTXT","
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis quam at ante tempor tempor. Mauris condimentum dignissim fringilla. Fusce ut enim id augue <a href='#' title='My title'>dignissim molestie</a>. Proin nec lacus non leo ullamcorper scelerisque vitae in arcu. Donec fermentum, leo ac euismod consequat, nisl arcu interdum nunc, pellentesque vestibulum velit lectus et ligula. Praesent quis purus nec enim porta porta. Nulla sodales mollis massa vitae ultrices. Nullam in dolor elit, eu vulputate ligula.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis quam at ante tempor tempor. Mauris condimentum dignissim fringilla. Fusce ut enim id augue dignissim molestie. Proin nec lacus non leo ullamcorper scelerisque vitae in arcu. Donec fermentum, leo ac euismod consequat, nisl arcu interdum nunc, pellentesque vestibulum velit lectus et ligula. Praesent quis purus nec enim porta porta. Nulla sodales mollis massa vitae ultrices. Nullam in dolor elit, eu vulputate ligula.</p>
+");
+define("_MD_XOSTYLE_TPLCLASS","Tpl class");
+define("_MD_XOSTYLE_TPLCLASSLINK","Tpl class with hypertext link");
+define("_MD_XOSTYLE_STYLECODE","Style for code");
+define("_MD_XOSTYLE_STYLEQUOTE","Style for quote");
+define("_MD_XOSTYLE_STYLEQUOTETXT","
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut <a href='#' title=''>aliquip</a> ex ea commodo Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+");
+define("_MD_XOSTYLE_TITFORM","Forms");
+define("_MD_XOSTYLE_CHEKME","Check me if you can");
+define("_MD_XOSTYLE_SUBMIT","Submit");
+define("_MD_XOSTYLE_GO","Go !");
+define("_MD_XOSTYLE_LEGENDTAG","legend form tag");
+define("_MD_XOSTYLE_RADBUTTON1","Radio button (.head)");
+define("_MD_XOSTYLE_RADBUTTON","Radio button");
+define("_MD_XOSTYLE_INPUTFILE1","Input File (label)");
+define("_MD_XOSTYLE_INPUTFILE2","Input File (.head)");
+define("_MD_XOSTYLE_INPUTSUBMIT1","Input Submit (label)");
+define("_MD_XOSTYLE_INPUTSUBMIT2","Input Submit (tr.foot)");
+define("_MD_XOSTYLE_INPUTFILE","Input File");
+define("_MD_XOSTYLE_INPUTSUBMIT","Input Submit");
+define("_MD_XOSTYLE_SELECT","Select Box (.head)");
+define("_MD_XOSTYLE_SELECT1","Select Box (label)");
+define("_MD_XOSTYLE_CHECKBOX","Checkbox (.head)");
+define("_MD_XOSTYLE_TEXTAREA","Textarea (.head)");
+define("_MD_XOSTYLE_TEXTAREA1","Textarea (label)");
+define("_MD_XOSTYLE_TEXTFIELD","Textfield (.head)");
+define("_MD_XOSTYLE_TEXTFIELD1","Textfield (label)");
+define("_MD_XOSTYLE_XOFORMCLASS","Forms with xoops .class");
+define("_MD_XOSTYLE_FORMTAG","Title form (th tag)");
 
 /* xostyle_smartytag.html */
 define("_MD_XOSTYLE_TITLE2","Smarty variables for XOOPS");

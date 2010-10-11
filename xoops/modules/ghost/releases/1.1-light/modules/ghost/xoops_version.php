eb53e38fd10cf6d0bae7febc036fda68086a9aa6
krisxoofoo <kris@xoofoo.org>
2010-10-11 21:40:44 +0000

    Ajout release module ghost 1.1 "light"

---
+++ xoops/modules/ghost/releases/1.1-light/modules/ghost/xoops_version.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Ghost module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package	Ghost
+ * @since		2.3.0
+ * @author 	kris <http://www.xoofoo.org>
+ * @version	$Id$
+**/
+
+$module_dirname = basename( dirname( __FILE__ ) ) ;
+
+$modversion["name"] 	=  _MI_GHOST_NAME;
+$modversion["version"] 	= 1.1;
+$modversion["description"] 	= _MI_GHOST_DESC;
+$modversion["author"] 	= "Kris - http://www.xoofoo.org";
+$modversion["credits"]	= "Chewb for idea - Xoops French Community";
+$modversion["help"] 		= "";
+$modversion["license"] 	= "GPL see LICENSE";
+$modversion["official"] 	= 1;
+$modversion["image"] 	= "images/" . $module_dirname . "_slogo.png";
+$modversion["dirname"] 	= $module_dirname;
+
+// All tables should not have any prefix!
+//$modversion["sqlfile"]["mysql"]	= "sql/mysql-" . $xoopsConfig["language"] . ".sql";
+//$modversion["tables"][0]        	= $module_dirname . "_firsttable";
+//$modversion["tables"][1]       	= $module_dirname . "_secondtable";
+
+// Admin Menu
+$modversion["hasAdmin"] 	= 0; // active = 1
+//$modversion["adminindex"] = "admin/index.php";
+//$modversion["adminmenu"] = "admin/menu.php";
+
+// Menu
+$modversion["hasMain"] 		= 1; // active = 1
+$modversion["sub"][1]["name"] 	= _MI_GHOST_SMNAME1;
+$modversion["sub"][1]["url"] 	= "index.php";
+$modversion["sub"][2]["name"] 	= _MI_GHOST_SMNAME2;
+$modversion["sub"][2]["url"] 	= "page2.php";
+
+// Templates
+$modversion["templates"][1]["file"] 		= $module_dirname . "_index.html";
+$modversion["templates"][1]["description"] 	= _MI_GHOST_DSCTPL1;
+
+$modversion["templates"][2]["file"] 		= $module_dirname . "_page2.html";
+$modversion["templates"][2]["description"] 	= _MI_GHOST_DSCTPL2;
+?>

27d77682d7c2a9c2783a4245878b7301282a4416
krisxoofoo <kris@xoofoo.org>
2010-10-11 21:32:52 +0000

    Ajout release module ghost 1.1

---
+++ xoops/modules/ghost/releases/1.1/modules/ghost/language/french_iso/main.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Ghost module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package	Ghost
+ * @since		2.3.0
+ * @author 	kris <http://www.xoofoo.org>
+ * @version	$Id$
+**/
+
+define("_MD_GHOST_TXT1" , "Mon premier texte avec le module <em>\"Ghost\"</em> pour Xoops.");
+define("_MD_GHOST_TXT2" , "Très bien ! et toi sur le sommet du Kilimandjaro ?");
+
+?>
---
+++ xoops/modules/ghost/releases/1.1/modules/ghost/language/french_iso/modinfo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Ghost module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package	Ghost
+ * @since		2.3.0
+ * @author 	kris <http://www.xoofoo.org>
+ * @version	$Id$
+**/
+
+// Module Info
+
+// The name of this module
+define("_MI_GHOST_NAME" , "Ghost");
+
+// A brief description of this module
+define("_MI_GHOST_DESC" , "Module Fantôme pour insérer facilement des pages personnelles dans XOOPS");
+
+// Main menu links
+define("_MI_GHOST_SMNAME1" , "Ma page 1");
+define("_MI_GHOST_SMNAME2" , "Ma page 2");
+
+// A brief description of these templates
+define("_MI_GHOST_DSCTPL1" , "Mon premier template avec le module Fantôme");
+define("_MI_GHOST_DSCTPL1" , "Mon second template avec le module Fantôme");
+
+// Admin menu links
+define("_MI_GHOST_ADMINDEX1" , "Lien Administration 1");
+
+?>

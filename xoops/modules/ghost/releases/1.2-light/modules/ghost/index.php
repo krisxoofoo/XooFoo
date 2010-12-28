0677f20e5f389edaf3cd69d5a1849dde412060f7
krisxoofoo <kris@xoofoo.org>
2010-12-28 02:28:54 +0000

    dépôt release 1.2 du module Ghost / Fantôme

---
+++ xoops/modules/ghost/releases/1.2-light/modules/ghost/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Ghost module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package	Ghost
+ * @since		2.3.0
+ * @author 	kris <http://www.xoofoo.org>
+ * @version	$Id: index.php 422 2010-10-11 21:26:51Z kris_fr $
+**/
+
+if ( !include("../../mainfile.php") ) {
+    die("XOOPS root path not defined");
+}
+$module_dirname = basename( dirname( __FILE__ ) ) ;
+
+$xoopsOption["template_main"] =  $module_dirname ."_index.html";
+
+include(XOOPS_ROOT_PATH."/header.php");
+
+if(isset($xoTheme) && is_object($xoTheme)) {
+   $xoopsTpl->assign("xoops_sitename","Put here the page title, visible at the top of the browser");
+   $xoopsTpl->assign("xoops_pagetitle", "Put here your page title");
+   $xoTheme->addMeta( "meta", "keywords", "Put here keyword, keyword1, keyword2, keyword3, etc.");
+   $xoTheme->addMeta( "meta", "description", "Put here the page description line that often appears in search results.");
+}
+global $xoTheme; 
+	$xoTheme->addStyleSheet("modules/" . $module_dirname . "/css/style.css");
+	//$xoTheme->addScript(XOOPS_URL . "/browse.php?Frameworks/jquery/jquery.js");
+	//$xoTheme->addScript("modules/" . $module_dirname . "/js/script.js");
+
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops/modules/ghost/releases/1.2-light/modules/ghost/templates/admin/ghost_admin_example.html
@@ -0,0 +1,24 @@
+<!-- $Id $ -->
+<div id="myid">
+
+	<!--  html code and define example -->
+	<h1>Html</h1>
+	<p>Bonjour le monde</p>
+	<p><{$smarty.const._AM_GHOST_TXT1}></p>
+	
+		<!-- or include extern html page -->
+		<object style="width:500px; height:auto; border: 0; overflow: hidden;" data="../include/mypageadmininhtml.html" type="text/html"></object>
+
+	<hr />
+
+	<!-- php code and define example -->
+	<{php}>
+		echo "<h1>Php</h1><h4>Plain text</h4>";
+		echo "<div><p>Hello World</p></div>";
+		echo _AM_GHOST_TXT1;
+	<{/php}>
+	
+		<!-- or include extern php page -->
+		<{include_php file='../include/mypageadmininphp.php'}>
+
+</div>
